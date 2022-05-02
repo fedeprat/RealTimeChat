760c94edd9f7a64870471f5e74a4a44a295e15d5
fedeprat <97050765+fedeprat@users.noreply.github.com>
2022-05-02 11:34:47 -0300

    script undefined fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-const socket = window.io();
+const socket = io();
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
 const roomName = document.getElementById('room-name')
